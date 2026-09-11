33b648ff50cf5602b66d226b1728e182974eb376
Sahil Rajput <sahilrajput03@gmail.com>
2026-09-11 18:37:24 +0530

    Update.

--- sahilrajput-bw-project/watchDevelopment.sh
+++ sahilrajput-bw-project/watchDevelopment.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Debug via `--verbose` of nodemon to know which files triggered restart and add
 #   unnecessary to --ignore option.
-nodemon -e java,xml,gradle --ignore ./app/src/main/res/xml/shortcuts.xml -x './gradlew installDebug && adb shell am start -n com.sahilrajput.twa/.LauncherActivity'
+nodemon -e java,xml,gradle \
+    --ignore ./app/src/main/res/xml/shortcuts.xml \
+    -x './gradlew installDebug && adb shell am start -n com.sahilrajput.twa/.LauncherActivity'
